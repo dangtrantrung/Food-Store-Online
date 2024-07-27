0c7e45fc6837b6f5a591d2c430002f994c25a3ad
dangtrantrung <trung280584@gmail.com>
2024-07-27 13:15:23 +0700

    Food APIs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { sample_foods } from './data'
+import { sample_foods, sample_tags } from './data'
 const app = express()
 app.use(
   cors({
@@ -15,12 +15,28 @@ app.get('/api/foods', (req, res) => {
 })
 app.get('/api/foods/search/:searchTerm', (req, res) => {
   const searchTerm = req.params.searchTerm
-  const foods=sample_foods.filter((food) =>
-  food.name.toLowerCase().includes(searchTerm.toLowerCase()),
-)
+  const foods = sample_foods.filter((food) =>
+    food.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
   res.send(foods)
 })
- 
+app.get('/api/foods/tags', (req, res) => {
+  //   res.send('Hello world!')
+  res.send(sample_tags)
+})
+app.get('/api/foods/tag/:tagName', (req, res) => {
+  //   res.send('Hello world!')
+  const tagName = req.params.tagName
+  const foods = sample_foods.filter((food) => food.tags?.includes(tagName))
+  res.send(foods)
+})
+app.get('/api/foods/:foodId', (req, res) => {
+  //   res.send('Hello world!')
+  const foodId = req.params.foodId
+  const food = sample_foods.find((food) => food.id == foodId) ?? 'Not Found'
+  res.send(food)
+})
+
 const port = 5000
 app.listen(port, () => {
   console.log('Website served on http://localhost:' + port)
